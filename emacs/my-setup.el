f7bf599badedfa4018c36d946b987a74ca23bf3f
Kang-min Liu <gugod@gugod.org>
2016-09-26 22:43:53 +0200

    settings.

---
+++ emacs/my-setup.el
@@ -0,0 +1,21 @@
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 8)
+(setq mouse-yank-at-point t)
+(setq inhibit-startup-message t)
+(setq visible-bell t)
+(setq scroll-margin 3
+      scroll-conservatively 10000)
+(show-paren-mode t)
+(setq show-paren-style 'parentheses)
+(setq frame-title-format "%b")
+(auto-image-file-mode)
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'LaTeX-hide-environment 'disabled nil)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-font-lock-mode nil)
+(setq-default font-lock-maximum-decoration t)
