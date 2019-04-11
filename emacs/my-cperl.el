3429fdcff730c6bca5048c41c892f071453d6988
Kang-min Liu <gugod@gugod.org>
2019-04-11 11:07:18 +0900

    t

--- emacs/my-cperl.el
+++ emacs/my-cperl.el
@@ -1,5 +1,5 @@
 (defalias 'perl-mode 'cperl-mode)
-(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\|t\\)\\'" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
