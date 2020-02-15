5e02619c0250e49f550f73d5bec09e482d46f1e2
Kang-min Liu <gugod@gugod.org>
2020-02-15 22:02:40 +0900

    a bunch of functions.

--- emacs/my-setup.el
+++ emacs/my-setup.el
@@ -2,6 +2,7 @@
 
 (set-language-environment 'UTF-8)
 (prefer-coding-system 'utf-8)
+
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'set-file-name-coding-system)
@@ -13,7 +14,27 @@
       initial-scratch-message ""
       inhibit-startup-message t
       require-final-newline t
-      delete-by-moving-to-trash IS-MAC)
+      delete-by-moving-to-trash IS-MAC
+      indent-tabs-mode nil
+      scroll-margin 3
+      default-tab-width 8
+      scroll-conservatively 10000
+      inhibit-startup-message t
+      mouse-yank-at-point t
+      show-paren-style 'parentheses
+      frame-title-format "%b"
+      browse-url-browser-function 'browse-url-default-macosx-browser
+      font-lock-maximum-decoration t)
+
+(if IS-MAC
+    (setq initial-frame-alist '((ns-appearance . "dark") (ns-transparent-titlebar . t))
+          mac-option-modifier 'meta
+          mac-command-modifier 'hyper))
+
+(let ((d (concat (getenv "HOME") "/.emacs.d/extra")))
+  (if (file-exists-p d)
+      (progn
+        (mapc 'load-file (directory-files d t ".el$" t)))))
 
 (use-package rg
   :bind ("C-#" . rg-dwim))
@@ -27,82 +48,63 @@
 (use-package yasnippet
   :config (yas-global-mode 1))
 
+(use-package ivy)
+
 (use-package dumb-jump
-  :config (dumb-jump-mode))
+  :after (ivy)
+  :config (dumb-jump-mode)
+  :custom (dumb-jump-selector 'ivy))
 
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C-S-c C-a" .   mc/edit-beginning-of-lines)
          ("C-S-c C-e" .   mc/edit-ends-of-lines)
-         ("C-S-c M-\"" .  mc/mark-all-like-this)
-         ))
-
-(use-package major-mode-icons
-  :ensure t
-  :config
-  (major-mode-icons-mode 1))
+         ("C-S-c M-\"" .  mc/mark-all-like-this)))
 
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
 
-(let ((d (concat (getenv "HOME") "/.emacs.d/extra")))
-  (if (file-exists-p d)
-      (progn
-        (mapc 'load-file (directory-files d t ".el$" t)))))
+(use-package multiple-cursors
+  :bind (("M-\"" . mc/mark-all-like-this)
+         ("C-S-c C-S-c" . mc/edit-lines)
+         ("C-S-c C-e" . mc/edit-ends-of-lines)
+         ("C-S-c C-a" . mc/edit-beginnings-of-lines)))
+
 
 (global-set-key (kbd "M-z") 'zap-up-to-char)
-
-(setq-default indent-tabs-mode nil
-              ;;; mode-line-format nil
-              scroll-margin 3
-              default-tab-width 8
-              scroll-conservatively 10000
-              inhibit-startup-message t
-              mouse-yank-at-point t
-              show-paren-style 'parentheses
-              frame-title-format "%b"
-              initial-frame-alist '((ns-appearance . "dark") (ns-transparent-titlebar . t))
-              browse-url-browser-function 'browse-url-default-macosx-browser
-              mac-option-modifier 'meta
-              mac-command-modifier 'hyper
-              font-lock-maximum-decoration t)
 
 (show-paren-mode t)
 (setq frame-title-format "%b")
 (auto-image-file-mode)
 
-(put 'set-goal-column 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'LaTeX-hide-environment 'disabled nil)
-(fset 'yes-or-no-p 'y-or-n-p)
+;; (put 'set-goal-column 'disabled nil)
+;; (put 'narrow-to-region 'disabled nil)
+;; (put 'upcase-region 'disabled nil)
+;; (put 'downcase-region 'disabled nil)
+;; (put 'LaTeX-hide-environment 'disabled nil)
+;; (fset 'yes-or-no-p 'y-or-n-p)
+
 (global-font-lock-mode nil)
 
-(global-set-key (kbd "s-{") 'previous-buffer)
-(global-set-key (kbd "s-}") 'next-buffer)
+;; (global-set-key (kbd "s-{") 'previous-buffer)
+;; (global-set-key (kbd "s-}") 'next-buffer)
 
-(defun on-after-init ()
-  (unless (display-graphic-p (selected-frame))
-    (progn
-      (set-face-foreground 'default "unspecified-fg" (selected-frame))
-      (set-face-background 'default "unspecified-bg" (selected-frame))
-      (if (fboundp 'window-system)
-          (if (window-system)
-              (progn
-                (global-unset-key (kbd "C-z")))
-            (progn
-              (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))))
-      )))
-(add-hook 'window-setup-hook 'on-after-init)
+;; (defun on-after-init ()
+;;   (unless (display-graphic-p (selected-frame))
+;;     (progn
+;;       (set-face-foreground 'default "unspecified-fg" (selected-frame))
+;;       (set-face-background 'default "unspecified-bg" (selected-frame))
+;;       (if (fboundp 'window-system)
+;;           (if (window-system)
+;;               (progn
+;;                 (global-unset-key (kbd "C-z")))
+;;             (progn
+;;               (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))))
+;;       )))
+;; (add-hook 'window-setup-hook 'on-after-init)
 
-(defun upcase-prev-word ()
-    (interactive)
-    (upcase-word -1))
-
-(global-set-key (kbd "M-U") 'upcase-prev-word)
 
 (let ((d (concat (getenv "HOME") "/Projects/gugod/bin")))
   (if (file-exists-p d)
@@ -110,5 +112,9 @@
         (add-to-list 'exec-path d)
         (setenv "PATH" (concat (getenv "PATH") ":" d))
         )))
+
+(require 'my-cperl)
+(require 'my-org)
+; (require 'my-mu4e)
 
 (provide 'my-setup)
