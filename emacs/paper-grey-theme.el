82c98d2b0a6904a056a84798086d7060c35afaa7
Kang-min Liu <gugod@gugod.org>
2020-04-09 11:37:56 +0800

    theme

--- emacs/paper-grey-theme.el
+++ emacs/paper-grey-theme.el
@@ -1,11 +1,20 @@
 (deftheme paper-grey
-  "Grey-scale theme emulating the look-en-feel of eink")
+  "Grayscale like eink")
 
 (custom-theme-set-faces
  'paper-grey
  '(fringe ((t (:background "#d0d0d1" :foreground "black"))))
- '(region ((t (:background "#e0e0e0" :distant-foreground "ns_selection_fg_color"))))
+ '(region ((t (:background "#c0c0c0" :distant-foreground "ns_selection_fg_color"))))
+ '(font-lock-comment-face ((t (:foreground "#909090"))))
+ '(font-lock-function-name-face ((t (:foreground "#222a33"))))
+ '(font-lock-builtin-face ((t (:foreground "#222a33"))))
+ '(font-lock-string-face ((t (:foreground "#222a33"))))
+ '(font-lock-type-face ((t (:foreground "#111a22"))))
+ '(font-lock-variable-name-face ((t (:foreground "#222a33"))))
+ '(cperl-array-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
+ '(cperl-hash-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
+ '(cperl-nonoverridable-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
  '(default ((t (:background "#d0d0d1" :foreground "#222a33"))))
- '(font-lock-comment-face ((t (:foreground "#909090")))))
+ '(font-lock-keyword-face ((t (:foreground "#333a33")))))
 
 (provide-theme 'paper-grey)
