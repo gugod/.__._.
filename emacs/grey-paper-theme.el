768d5bcfe29ac2cb54190d23504243a2a4dd5f57
Kang-min Liu <gugod@gugod.org>
2020-04-12 14:27:43 +0800

    theme.

--- emacs/grey-paper-theme.el
+++ emacs/grey-paper-theme.el
@@ -3,19 +3,20 @@
 
 (custom-theme-set-faces
  'grey-paper
- '(link ((t (:foreground "#222a53"))))
- '(cperl-array-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
- '(cperl-hash-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
- '(cperl-nonoverridable-face ((t (:background "#d0d0d1" :foreground "#222a33"))))
- '(default ((t (:background "#d0d0d1" :foreground "#222a33"))))
+ '(link ((t (:foreground "#224022" :underline t))))
+ '(cperl-array-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
+ '(cperl-hash-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
+ '(cperl-nonoverridable-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
+ '(default ((t (:background "#d0d1d0" :foreground "#222a33"))))
  '(font-lock-builtin-face ((t (:foreground "#222a33"))))
  '(font-lock-comment-face ((t (:foreground "#909090"))))
- '(font-lock-function-name-face ((t (:foreground "#222a33"))))
+ '(font-lock-constant-face ((t (:foreground "#111111"))))
+ '(font-lock-function-name-face ((t (:foreground "#222a33" :bold t))))
  '(font-lock-keyword-face ((t (:foreground "#333a33"))))
  '(font-lock-string-face ((t (:foreground "#222a33"))))
  '(font-lock-type-face ((t (:foreground "#111a22"))))
  '(font-lock-variable-name-face ((t (:foreground "#222a33"))))
- '(fringe ((t (:background "#d0d0d1" :foreground "black"))))
+ '(fringe ((t (:background "#d0d1d0" :foreground "black"))))
  '(region ((t (:background "#c0c0c0" :distant-foreground "ns_selection_fg_color"))))
  )
 
