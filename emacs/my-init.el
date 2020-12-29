30ce6f0b431a2244cda1007317502630415ceb48
Kang-min Liu <gugod@gugod.org>
2020-12-29 09:20:32 +0900

    .

--- emacs/my-init.el
+++ emacs/my-init.el
@@ -21,7 +21,8 @@
 
 (require 'use-package)
 
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t
+      use-package-always-defer t)
 
 (use-package kotlin-mode)
 (use-package elm-mode)
